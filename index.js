90e9b59354a39b417ae8d5e7e3114b54ee81b820
Saidnazar-Abdullayev <abdullayevsaidnazar872@gmail.com>
2023-03-03 12:34:23 +0500

    fix

---
+++ index.js
@@ -0,0 +1,40 @@
+var a = +prompt("1-raqam kiriting")
+var b =  prompt("amalni kiriting")
+var c = +prompt("2-raqam kiriting") 
+    
+if (b  == "+"){
+  console.log(a + c);
+}else if (b  == "-"){
+  console.log(a - c);
+}else if (b == "*"){
+  console.log(a * c);
+}else if (b  == "/"){
+  console.log(a / c);
+}else {
+  console.log("bunday amal yo'q");
+}
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
